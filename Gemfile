eef01b3cac92bdbaa725240ec9d0485e1fc454eb
Brian Arenz <brianarenz@gmail.com>
2014-02-04 09:36:42 -0600

    Add artii gem and header

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 
 gem 'edmunds_ruby', '~> 1.0.2'
 gem 'sqlite3-ruby', '~> 1.3.3'
+gem "artii", '~> 2.0.4'
--- lib/output_main.rb
+++ lib/output_main.rb
@@ -1,11 +1,14 @@
+require 'artii'
+
 def clear_screen
   puts "\e[H\e[2J"
 end
 
 def header_main
   clear_screen
-  puts blue("RUBY AUTO REPAIR TRACKER")
-  puts blue("========================")
+  artii = Artii::Base.new :font => 'slant'
+  header = artii.asciify('AUTO REPAIR')
+  puts blue(header)
 end
 
 def delete?
